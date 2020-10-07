72bc8f48adaab0bcd42bbf7ccdaf8365bfbc3c00
tawebbcn <ta-web-bcn@ironhack.com>
2020-10-07 14:08:44 +0200

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,22 @@
+// PROMISES
+
+// Iteración 1
+
+// Definimos dos variables para utilizar los endpoints de cada API
+
+const astroUrl = "http://api.open-notify.org/astros.json";
+const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
+
+function getProfiles() {
+  // Aquí escribiremos la función que hará las llamadas a las API
+}
+
+// Iteración 2
+
+function generateHTML() {
+  // Aquí escribiremos la función que generará los elementos HTML que mostraremos
+}
+
+// Iteración 3
+
+// Aquí escribiremos el addEventListener que 'escuchará' a nuestro botón
